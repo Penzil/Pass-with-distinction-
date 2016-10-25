6ea9453192488c2d2a82a724340c754400287892
Martin Palman <martin@ellva.se>
2016-10-25 09:04:55 +0200

    second commit

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,12 +6,17 @@ import java.awt.Color;
 import javax.swing.JFrame;
 import jaco.mp3.player.MP3Player;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+
 import javax.swing.ImageIcon;
+
 public class MainFrame {
 
 	private JFrame frmMyMusicPlayer;
-	
+	private MP3Player mp3Player = new MP3Player();
+	JLabel lblNewLabel = new JLabel("Music:");
+
 	/**
 	 * Launch the application.
 	 */
@@ -45,55 +50,70 @@ public class MainFrame {
 		frmMyMusicPlayer.setTitle("My Music Player");
 		frmMyMusicPlayer.setBounds(100, 100, 290, 275);
 		frmMyMusicPlayer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JButton btnPlay = new JButton("");
 		btnPlay.setBackground(Color.WHITE);
 		btnPlay.setIcon(new ImageIcon("C:\\Users\\Martin\\Desktop\\Nackademin\\icons\\playp.jpg"));
 		btnPlay.setBounds(12, 104, 97, 25);
 		btnPlay.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				new MP3Player(new File("C:\\Users\\Martin\\Desktop\\Nackademin\\Crazy Frog - Axel F.mp3")).play();
-				
+				System.out.println("I clicked the button play");
+				// new MP3Player(new
+				// File("C:\\Users\\Martin\\Desktop\\Nackademin\\Crazy Frog -
+				// Axel F.mp3")).play();
+				mp3Player.play();
 			}
 		});
 		frmMyMusicPlayer.getContentPane().setLayout(null);
 		frmMyMusicPlayer.getContentPane().add(btnPlay);
-		
+
 		JButton btnStop = new JButton("");
 		btnStop.setBackground(Color.WHITE);
 		btnStop.setIcon(new ImageIcon("C:\\Users\\Martin\\Desktop\\Nackademin\\icons\\stopp.jpg"));
 		btnStop.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new MP3Player(new File("C:\\Users\\Martin\\Desktop\\Nackademin\\Crazy Frog - Axel F.mp3")).stop();
+				System.out.println("I clicked the button stop");
+				//new MP3Player(new File("C:\\Users\\Martin\\Desktop\\Nackademin\\Crazy Frog - Axel F.mp3")).stop();
+				mp3Player.stop();
 			}
 		});
 		btnStop.setBounds(12, 168, 97, 25);
 		frmMyMusicPlayer.getContentPane().add(btnStop);
-		
+
 		JButton btnPause = new JButton("");
 		btnPause.setBackground(Color.WHITE);
 		btnPause.setIcon(new ImageIcon("C:\\Users\\Martin\\Desktop\\Nackademin\\icons\\pausep.jpg"));
 		btnPause.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new MP3Player(new File("C:\\Users\\Martin\\Desktop\\Nackademin\\Crazy Frog - Axel F.mp3")).pause();
+				System.out.println("I clicked the button pause");
+				//new MP3Player(new File("C:\\Users\\Martin\\Desktop\\Nackademin\\Crazy Frog - Axel F.mp3")).pause();
+				mp3Player.pause();
 			}
 		});
 		btnPause.setBounds(163, 104, 97, 25);
 		frmMyMusicPlayer.getContentPane().add(btnPause);
-		
+
 		JButton btnOpen = new JButton("");
 		btnOpen.setBackground(Color.WHITE);
 		btnOpen.setIcon(new ImageIcon("C:\\Users\\Martin\\Desktop\\Nackademin\\icons\\openp.jpg"));
 		btnOpen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				System.out.println("I clicked the button open!");
+
+				JFileChooser fileChooser = new JFileChooser();
+				int returnVal = fileChooser.showOpenDialog(btnOpen);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fileChooser.getSelectedFile();
+					mp3Player = new MP3Player(file);
+					System.out.println(file.getAbsolutePath());
+					lblNewLabel.setText(file.getName().toString());
+				}
 			}
 		});
 		btnOpen.setBounds(163, 168, 97, 25);
 		frmMyMusicPlayer.getContentPane().add(btnOpen);
-		
-		JLabel lblNewLabel = new JLabel("Music:");
-		lblNewLabel.setBounds(110, 13, 50, 16);
+
+		lblNewLabel.setBounds(12, 13, 248, 16);
 		frmMyMusicPlayer.getContentPane().add(lblNewLabel);
 	}
 }
